1a4c68efd6c0e66a394433e226b471fe61cd85dd
Ankit Patil <ankitpatil.v@gmail.com>
2022-07-06 13:31:25 +0530

    v1.0

---
+++ test.py
@@ -0,0 +1,7 @@
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+img = cv2.imread('2.JPG')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+print ("Number of Peoples in Images ")
+print (len(faces))
